21e86fe1830a0823dd7ea6ccb2ab3f8512bb064e
Adrian Cooper <adriancooper81@hotmail.com>
2021-09-27 17:11:05 +0930

    Update Get-ScoopPackages.ps1

--- functions/Get-ScoopPackages.ps1
+++ functions/Get-ScoopPackages.ps1
@@ -1,9 +1,36 @@
-function Get-ScoopPackages
-{
+<#
+.SYNOPSIS
+    Get the currently install scoop packages
+.DESCRIPTION
+    Get the current installed packages using the scoop package manager https://scoop.sh
+.EXAMPLE
+    PS C:\> Get-ScoopPackage
+    This example will return the currently install scoop packages.
+
+    PS C:\> Get-ScoopPackage "Install"
+    This example will return the currently install scoop packages with Install in the name.
+.INPUTS
+    Optional String $searchstring
+.OUTPUTS
+    System.String
+.NOTES
+    
+#>
+function Get-ScoopPackages {
     [CmdletBinding()]
     param (
-        [stiring]$searchstring = '*'
+        [string]$searchstring = ''
     )
-
-    Invoke-Command(scoop list $searchstring)
+    
+    begin {
+        
+    }
+    
+    process {
+        $result = (powershell scoop list $searchstring) | Out-String
+    }
+    
+    end {
+        return $result
+    }
 }
