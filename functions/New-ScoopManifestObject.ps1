7c61fd791792f929d178e5bac4b239adaea611b5
Adrian Cooper <adriancooper81@hotmail.com>
2021-09-27 22:50:00 +0930

    Create New-ScoopManifestObject.ps1

---
+++ functions/New-ScoopManifestObject.ps1
@@ -0,0 +1,106 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+
+function New-ScoopManifestObject {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Position = 0,
+            Mandatory = $true,
+            HelpMessage = "Manifest ID"
+        )]
+        [String]
+        $ID,
+
+        [Parameter(
+            Position = 1,
+            Mandatory = $true,
+            HelpMessage = "Manifest Bucket"
+        )]
+        [String]
+        $Bucket,
+
+        [Parameter(
+            Position = 2,
+            Mandatory = $false,
+            HelpMessage = "Manifest Name"
+        )]
+        [String]
+        $Name,
+
+        [Parameter(
+            Position = 3,
+            Mandatory = $true,
+            HelpMessage = "Manifest Version"
+        )]
+        [String]
+        $Version,
+
+        [Parameter(
+            Position = 4,
+            Mandatory = $false,
+            HelpMessage = "Manifest Description"
+        )]
+        [String]
+        $Description,
+
+        [Parameter(
+            Position = 5,
+            Mandatory = $false,
+            HelpMessage = "Manifest Website"
+        )]
+        [String]
+        $Website,
+
+        [Parameter(
+            Position = 6,
+            Mandatory = $false,
+            HelpMessage = "Manifest License"
+        )]
+        [String]
+        $License,
+
+        [Parameter(
+            Position = 7,
+            Mandatory = $false,
+            HelpMessage = "Manifest Path"
+        )]
+        [String]
+        $Manifest,
+
+        [Parameter(
+            Position = 8,
+            Mandatory = $false,
+            HelpMessage = "Manifest Binaries"
+        )]
+        [String]
+        $Binaries
+    )
+    
+    $obj = [PSCustomObject]@{
+        ID = $ID
+        Bucket = $Bucket
+        Name = $Name
+        Version = $Version
+        Description = $Description
+        Website = $Website
+        License = $License
+        Path = $Path
+        Binaries = $Binaries
+    }
+
+    return $obj
+}
