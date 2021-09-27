9e751adc5e2cd2ac88579c4d411c90137e1f65a1
Adrian Cooper <adriancooper81@hotmail.com>
2021-09-27 17:38:09 +0930

    Update Get-ScoopPackages.ps1

--- functions/Get-ScoopPackages.ps1
+++ functions/Get-ScoopPackages.ps1
@@ -6,11 +6,11 @@
 .EXAMPLE
     PS C:\> Get-ScoopPackage
     This example will return the currently install scoop packages.
-
+.EXAMPLE
     PS C:\> Get-ScoopPackage "Install"
     This example will return the currently install scoop packages with Install in the name.
 .INPUTS
-    Optional String $searchstring
+    Optional String $Filter
 .OUTPUTS
     System.String
 .NOTES
@@ -19,7 +19,21 @@
 function Get-ScoopPackages {
     [CmdletBinding()]
     param (
-        [string]$searchstring = ''
+        # Filter the currently installed packages by manifest ID
+        [Parameter(
+            Position = 0,
+            Mandatory = $false
+        )]
+        [String]
+        $Filter,
+
+        # Return packages that are installed globaly
+        [Parameter(
+            Position = 1,
+            Mandatory = $false
+        )]
+        [Switch]
+        $Global = $false
     )
     
     begin {
@@ -27,7 +41,7 @@ function Get-ScoopPackages {
     }
     
     process {
-        $result = (powershell scoop list $searchstring) | Out-String
+        $result = (powershell scoop list $Filter) | Out-String
     }
     
     end {
