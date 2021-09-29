25a5bb5712fa2bfd2f0b25401341f1033eed564b
Adrian Cooper <adriancooper81@hotmail.com>
2021-09-29 20:33:43 +0930

    Update PSScoop.psm1

--- PSScoop.psm1
+++ PSScoop.psm1
@@ -2,10 +2,15 @@
 $functionpath = $PSScriptRoot + "\functions\"
 
 #   Get a list of all the function file names...
-$functionlist = Get-ChildItem -Path $functionpath -Name
+# $functionlist = Get-ChildItem -Path $functionpath -Name
+$functionlist = (Get-ChildItem -Path $functionpath).Name
 
 #   Loop over all the files and dot source them into memory...
 foreach ($function in $functionlist)
 {
-    ($functionpath + $function)
+    . ($functionpath + $function)
 }
+
+# Functions to export from this module
+$functions = @('Update-Scoop','Get-ScoopApps')
+Export-ModuleMember -Function $functions
