b5a40d36cabb40540f3db2670361e290b2658026
Adrian Cooper <adriancooper81@hotmail.com>
2021-10-03 00:00:37 +0930

    Create Get-Scoop.ps1

---
+++ functions/Get-Scoop.ps1
@@ -0,0 +1,100 @@
+<#
+.SYNOPSIS
+    Get the currently install scoop packages
+.DESCRIPTION
+    Get the current installed packages using the scoop package manager https://scoop.sh
+.EXAMPLE
+    PS C:\> Get-Scoop
+    This example will return the currently install scoop packages.
+.INPUTS
+    Optional String $Filter
+.OUTPUTS
+    System.String
+.NOTES
+    
+#>
+function Get-Scoop {
+    [CmdletBinding()]
+    param (
+        [Parameter(
+            Position = 1,
+            Mandatory = $false,
+            HelpMessage = "Include packages that are installed Globaly"
+        )]
+        [Switch]
+        $GlobalApps = $false,
+
+        [Parameter(
+            Position = 2,
+            Mandatory = $false,
+            HelpMessage = "Include packages that are installed as current user"
+        )]
+        [Switch]
+        $UserApps = $false
+    )
+    
+    begin {
+        # $objs = $null
+        # $objs = '{"key": "value"}' | ConvertFrom-Json -AsHashtable
+        $objs = @{}
+        # Write-Verbose $objs.Keys
+        # $objs.Clear()
+
+        # $obj.GetEnumerator() | ForEach-Object{if($_.key -like "*Install*"){Write-Output $_.key; Write-Output $_.Value}}
+        $userPath = "$env:UserProfile\scoop\apps"
+        # Write-Verbose $userPath
+        $globalPath = "C:\ProgramData\scoop\apps"
+        # Write-Verbose $globalPath
+    }
+    
+    process {
+        if ($GlobalApps -eq $false -and $UserApps -eq $false){
+            # Write-Verbose "Both false"
+            $GlobalApps = $true
+            $UserApps = $true
+        }
+        if ($UserApps -eq $true){
+            # Write-Verbose "User true"
+            foreach ($folder in (Get-ChildItem -Path $userPath)){
+                # Write-Verbose $folder
+                $obj = @{}
+                $manifestPath = $folder.ToString() + "\current\scoop-manifest.json"
+                # Write-Verbose $manifestPath
+                if (Test-Path -Path $manifestPath){
+                    $obj = New-ScoopManifestObject -Path $manifestPath
+                    $currentId = $obj.id
+                    # Write-Verbose $obj
+                    # Write-Verbose $currentId
+                    $objs.Add($currentId, $obj)
+                    # $objs.$currentId = $obj
+                }
+            }
+        }
+        if ($GlobalApps -eq $true){
+            # Write-Verbose "Global true"
+            foreach ($folder in (Get-ChildItem -Path $globalPath)){
+                $obj = @{}
+                $manifestPath = $folder.ToString() + "\current\scoop-manifest.json"
+                if (Test-Path -Path $manifestPath){
+                    $obj = New-ScoopManifestObject -Path $manifestPath
+                    $currentId = $obj.id
+                    # Write-Verbose $currentId
+                    if ($objs.Contains($currentId)){
+                        Write-Verbose "$currentId is installed in the user profile."
+                    }
+                    else{
+                        $objs.Add($currentId, $obj)
+                        # $objs.$currentId = $obj
+                    }
+                }
+            }
+        }
+        # Write-Verbose "This function has run"
+    }
+    
+    end {
+        # $objs = $objs.GetEnumerator() | Sort-Object Name
+        # Write-Verbose $objs.Keys
+        return $objs
+    }
+}#Get-ScoopPackages
